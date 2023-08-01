882a0029e74a05187aa61c23b7ffe1e0d82e3b3e
candy82202 <candy82202@yahoo.com.tw>
2023-08-01 17:41:01 +0800

    [V] SierraCore ConnectionString

---
+++ SIERRA_Server/SIERRA_Server/Models/DTOs/DessertDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace SIERRA_Server.Models.DTOs
+{
+    public class DessertDTO
+    {
+        public int DessertId { get; set; }
+
+        public string DessertName { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public int? UnitPrice { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Status { get; set; }
+
+        public DateTime? CreateTime { get; set; }
+    }
+}
---
+++ SIERRA_Server/SIERRA_Server/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using SIERRA_Server.Models.EFModels;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(Options => {
+    Options.UseSqlServer(builder.Configuration.GetConnectionString("Sierra"));
+    });
+
+builder.Services.AddControllers();
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseRouting();
+
+// Allow CORS
+app.UseCors("AllowOrigin");
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
