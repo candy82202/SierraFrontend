1477f6a8ec2197ec7f7b914dea935e718aa98706
JamesHsieh1102 <casear1102@gmail.com>
2023-08-07 14:33:07 +0800

    merge teacher

---
+++ SIERRA_Server/SIERRA_Server/Models/DTOs/Lessons/TeacherDTO.cs
@@ -0,0 +1,8 @@
+﻿using SIERRA_Server.Models.EFModels;
+
+namespace SIERRA_Server.Models.DTOs.Lessons {
+    public class TeacherDTO {
+        public List<Teacher>? Teachers { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
