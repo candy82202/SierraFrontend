2315e3061b72806efb064620457ce5eed29974e4
leianzhan <leianzhan@gmail.com>
2023-08-09 16:28:54 +0800

    feat:訂單dto

--- SIERRA_Server/SIERRA_Server/Models/DTOs/Orders/CreateDessertOrderDTO.cs
+++ SIERRA_Server/SIERRA_Server/Models/DTOs/Orders/CreateDessertOrderDTO.cs
@@ -2,8 +2,10 @@
 {
     public class CreateDessertOrderDTO
     {
+        public List<ItemDto> Items { get; set; }
         public int Id { get; set; }
         public int MemberId { get; set; }
+        public string Username { get; set; }
         public int DessertOrderStatusId { get; set; }
         public int? MemberCouponId { get; set; }
         public DateTime CreateTime { get; set; }
@@ -14,6 +16,21 @@
         public int DessertOrderTotal { get; set; }
         public string DeliveryMethod { get; set; }
         public string Note { get; set; }
+        public string PayMethod { get; set; }
+
         public string DiscountInfo { get; set; }
+    }
+    public class ItemDto
+    {
+        // 甜點明細
+        //public int Id { get; set; }
+        //public int SpecificationId { get; set; }
+        //public int DessertOrderId { get; set; }
+        //public int DessertId { get; set; }
+        public string DessertName { get; set; }
+        public int Quantity { get; set; }
+        public int UnitPrice { get; set; }
+        public int Subtotal { get; set; }
+
     }
 }
