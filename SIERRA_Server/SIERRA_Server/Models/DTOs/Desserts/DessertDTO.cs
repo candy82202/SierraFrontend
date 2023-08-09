f8a1fca460175cf3898329d69eaa0cc4a9b684c3
candy82202 <candy82202@yahoo.com.tw>
2023-08-09 13:44:32 +0800

    feat: Get Dessert id add Specification

--- SIERRA_Server/SIERRA_Server/Models/DTOs/Desserts/DessertDTO.cs
+++ SIERRA_Server/SIERRA_Server/Models/DTOs/Desserts/DessertDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace SIERRA_Server.Models.DTOs.Desserts
+﻿using SIERRA_Server.Models.EFModels;
+
+namespace SIERRA_Server.Models.DTOs.Desserts
 {
     public class DessertDTO
     {
@@ -8,12 +10,23 @@
 
         public string CategoryName { get; set; }
 
-        public int? UnitPrice { get; set; }
+        //public int? UnitPrice { get; set; }
 
         public string Description { get; set; }
 
         public string DessertImageName { get; set; }
-
-
+        //public Specification Specification { get; set; }
+        //public string Flavor { get; set; }
+        //public string Size { get; set; }
+        public List<SpecificationDTO> Specifications { get; set; } // List of SpecificationDTO objects
+     
+    }
+    public class SpecificationDTO
+    {
+        public int SpecificationId { get; set; }
+        public string Size { get; set; }
+        public string Flavor { get; set; }
+        public int? UnitPrice { get; set; }
+        // Include other properties here
     }
 }
