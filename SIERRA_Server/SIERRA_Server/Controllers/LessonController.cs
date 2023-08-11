831dec8c0ea6b26a42f0dc752fa439e9e4295925
JamesHsieh1102 <casear1102@gmail.com>
2023-08-11 10:39:20 +0800

    feat:修改UnitLessonDTO和GetLessonById，新增抓取資料量

--- SIERRA_Server/SIERRA_Server/Controllers/LessonController.cs
+++ SIERRA_Server/SIERRA_Server/Controllers/LessonController.cs
@@ -4,104 +4,113 @@ using SIERRA_Server.Models.DTOs.Lessons;
 using SIERRA_Server.Models.EFModels;
 using System.Diagnostics;
 
-<<<<<<< HEAD
+
 namespace SIERRA_Server.Controllers {
 
-=======
-namespace SIERRA_Server.Controllers
-{
->>>>>>> b9dd25e372834b548041611a37762b58e8fa160d
-    [Route("api/[controller]")]
-    [ApiController]
-    public class LessonController : ControllerBase {
 
-        private readonly AppDbContext _context;
-        public LessonController(AppDbContext context)
-        {
-            _context = context;
+    namespace SIERRA_Server.Controllers {
+
+        [Route("api/[controller]")]
+        [ApiController]
+        public class LessonController : ControllerBase {
+
+            private readonly AppDbContext _context;
+            public LessonController(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            [HttpGet]
+            public async Task<ActionResult<LessonDTO>> GetLessons(string? categoryName)
+            {
+                if (_context == null)
+                {
+                    return NotFound();
+                }
+
+                var lessons = _context.Lessons.Include(t => t.Teacher)
+                                                                  .Include(lm => lm.LessonImages)
+                                                                  .Include(lc => lc.LessonCategory)
+                                                                  .Where(l => l.Teacher.TeacherStatus == true && l.LessonStatus == true)
+                                                                  .AsQueryable();
+
+
+                if (!string.IsNullOrEmpty(categoryName))
+                {
+                    lessons = lessons.Include(l => l.LessonCategory)
+                                                .Where(lc => lc.LessonCategory.LessonCategoryName.Contains(categoryName));
+                }
+
+                LessonDTO lessondto = new LessonDTO();
+                lessondto.Lessons = await lessons.ToListAsync();
+
+                return lessondto;
+            }
+
+            // GET:category
+
+            [HttpGet("category")]
+            public async Task<ActionResult<LessonCategoryDTO>> GetLessonCategories()
+            {
+                if (_context == null)
+                {
+                    return NotFound();
+                }
+
+                var lessonCategories = _context.LessonCategories.Include(l => l.Lessons);
+
+
+                var lessonCategoryDTO = new LessonCategoryDTO();
+                lessonCategoryDTO.Categories = await lessonCategories.Select(lc => new LessonCategoryDTOItem
+                {
+                    LessonCategoryId = lc.LessonCategoryId,
+                    LessonCategoryName = lc.LessonCategoryName
+                }).ToListAsync();
+
+                return lessonCategoryDTO;
+            }
+
+            [HttpGet("lessonId")]
+            public async Task<ActionResult<UnitLessonDTO>> GetLessonById(int lessonId)
+            {
+                if (_context == null)
+                {
+                    return NotFound();
+                }
+
+                var lesson = await _context.Lessons.Include(t => t.Teacher)
+                                                                        .Include(lm => lm.LessonImages)
+                                                                        .Include(lc => lc.LessonCategory)
+                                                                        .FirstOrDefaultAsync(l => l.LessonId == lessonId);
+
+                if (lesson == null)
+                {
+                    return NotFound();
+                }
+
+                var lessonDTO = new UnitLessonDTO
+                {
+                    LessonId = lesson.LessonId,
+                    LessonDessert = lesson.LessonDessert,
+                    LessonImages = lesson.LessonImages,
+                    LessonTitle = lesson.LessonTitle,
+                    LessonDetail = lesson.LessonDetail,
+                    LessonInfo = lesson.LessonInfo,
+                    LessonPrice = lesson.LessonPrice,
+                    LessonEndTime = lesson.LessonEndTime,
+                    LessonHours = lesson.LessonHours,
+                    LessonStatus = lesson.LessonStatus,
+                    LessonTime  = lesson.LessonTime,
+                    ActualCapacity  = lesson.ActualCapacity,
+                    CreateTime = lesson.CreateTime,
+                    MaximumCapacity = lesson.MaximumCapacity,
+                };
+
+                return lessonDTO;
+            }
+
+
         }
-        
-        [HttpGet]
-        public async Task<ActionResult<LessonDTO>> GetLessons(string? categoryName)
-        {
-            if (_context == null)
-            {
-                return NotFound();
-            }
-
-            var lessons =  _context.Lessons.Include(t => t.Teacher)
-                                                              .Include(lm => lm.LessonImages)
-                                                              .Include(lc => lc.LessonCategory)
-                                                              .Where(l => l.Teacher.TeacherStatus == true && l.LessonStatus == true)
-                                                              .AsQueryable();
-          
-
-            if (!string.IsNullOrEmpty(categoryName))
-            {
-                lessons = lessons.Include(l => l.LessonCategory)
-                                            .Where(lc => lc.LessonCategory.LessonCategoryName.Contains(categoryName));
-            }
-
-            LessonDTO lessondto = new LessonDTO();
-            lessondto.Lessons = await lessons.ToListAsync();
-
-            return lessondto;
-        }
-
-        // GET:category
-      
-        [HttpGet("category")]
-        public async Task<ActionResult<LessonCategoryDTO>> GetLessonCategories()
-        {
-            if (_context == null)
-            {
-                return NotFound();
-            }
-
-            var lessonCategories = _context.LessonCategories.Include(l => l.Lessons);
-                                                                               
-
-            var lessonCategoryDTO = new LessonCategoryDTO();
-            lessonCategoryDTO.Categories = await lessonCategories.Select(lc => new LessonCategoryDTOItem
-                                                                                                                                   {
-                                                                                                                                        LessonCategoryId = lc.LessonCategoryId,
-                                                                                                                                        LessonCategoryName = lc.LessonCategoryName
-                                                                                                                                    }).ToListAsync();
-
-            return lessonCategoryDTO;
-        }
-
-        [HttpGet("lessonId")]
-        public async Task<ActionResult<UnitLessonDTO>> GetLessonById(int lessonId)
-        {
-            if (_context == null)
-            {
-                return NotFound();
-            }
-
-            var lesson = await _context.Lessons.Include(t => t.Teacher)
-                                                                    .Include(lm => lm.LessonImages)
-                                                                    .Include(lc => lc.LessonCategory)
-                                                                    .FirstOrDefaultAsync(l =>l.LessonId == lessonId);
-
-            if(lesson == null)
-            {
-                return NotFound();
-            }
-
-            var lessonDTO = new UnitLessonDTO
-            {
-                LessonId = lesson.LessonId,
-                LessonDessert = lesson.LessonDessert,
-                LessonImages = lesson.LessonImages,
-                LessonTitle = lesson.LessonTitle,
-                LessonDetail = lesson.LessonDetail,
-                LessonInfo = lesson.LessonInfo,
-                LessonPrice = lesson.LessonPrice,
-            };
-
-            return lessonDTO;
-        }
-
     }
 }
+
