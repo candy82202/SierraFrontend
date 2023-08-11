e7e9cb4389957871f5cc4f03f34351dbfe018dc9
candy82202 <candy82202@yahoo.com.tw>
2023-08-11 10:42:55 +0800

    feat : AppDbContext delete warning words

--- SIERRA_Server/SIERRA_Server/Models/EFModels/AppDbContext.cs
+++ SIERRA_Server/SIERRA_Server/Models/EFModels/AppDbContext.cs
@@ -51,7 +51,7 @@ namespace SIERRA_Server.Models.EFModels
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+
                 optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Sierra0801;Persist Security Info=True;User ID=sa5;Password=sa5");
             }
         }
